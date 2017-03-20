ee0331458e8acb9dc7499b3078ef813d7dd8c993
Ozaynaci <hsnzync_@hotmail.com>
2017-03-20 23:06:02 +0100

    landings page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,12 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <link rel="icon" href="/webApplication/resources/assets/img/favicon.ico" type="image/x-icon"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Buurtbord Landingpage</title>
 
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="/webApplication/resources/assets/css/bootstrap.min.css" rel="stylesheet">
         <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="/webApplication/resources/assets/css/style.css" type="text/css">
 
@@ -14,17 +15,21 @@
         <div class="container">
             <div class="content">
                 <div class="header">
-
+                    <div class="rectangle"></div>
+                    <div class="logo_header"></div>
                 </div>
 
                 <div class="image">
+                    <div class="rectangle_intro">
+                        <h3 class="intro_text">Buurtbord Hilligersberg - Schiebroek</h3>
+                        <p class="intro_subtext">Tik op het scherm om de plannen voor de buurt te bekijken.</p>
+                    </div>
 
                 </div>
             </div>
         </div>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+        <script src="/webApplication/resources/assets/js/bootstrap.min.js"></script>
     </body>
 </html>
