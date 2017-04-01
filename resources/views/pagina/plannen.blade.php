d1e5cbce047d20d20968481d8bb372f5afe98ec9
Ozaynaci <hsnzync_@hotmail.com>
2017-04-01 21:52:03 +0200

    landingspage laravel

--- resources/views/pagina/plannen.blade.php
+++ resources/views/pagina/plannen.blade.php
@@ -1,17 +1,6 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="icon" href="/webApplication/resources/assets/img/favicon.ico" type="image/x-icon"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Buurtbord Landingpage</title>
+@extends('layouts.app')
 
-        <link href="/webApplication/resources/assets/css/bootstrap/bootstrap.min.css" rel="stylesheet">
-        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="{{ elixir('css/app.css') }}" type="text/css">
-
-    </head>
-    <body id="plannen"> <!-- redirect -->
+@section('content')
         <div class="container">
             <div class="content">
                 <div class="header">
@@ -54,8 +43,5 @@
 
             </div>
         </div>
+@endsection
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="/webApplication/resources/assets/js/bootstrap/bootstrap.min.js"></script>
-    </body>
-</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+    <div class="container">
+        <div class="content">
+            <div class="header">
+                <div class="rectangle"></div>
+                <div class="logo_header"></div>
+            </div>
 
-                <div class="panel-body">
-                    Your Application's Landing Page.
+            <div class="image_landing">
+                <div class="rectangle_intro">
+                    <h3 class="intro_text">Buurtbord Hilligersberg - Schiebroek</h3>
+                    <p class="intro_subtext">Log in om te reageren op plannen van de gemeente en burgers.</p>
+
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-12 control-label">Email-adres:</label>
+
+                            <div class="col-md-11">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-11 control-label">Wachtwoord:</label>
+
+                            <div class="col-md-11">
+                                <input id="password" type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <button type="submit" class="btn btn-primary login_btn">Login
+                                <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                                </button>
+
+                                <!-- <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a> -->
+                            </div>
+                        </div>
+                    </form>
+
                 </div>
+
             </div>
         </div>
     </div>
-</div>
 @endsection
